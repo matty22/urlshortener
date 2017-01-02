4188401a4dc350449a01dab0e7c1c7fa8b102257
Matt <matt.leonard.22@gmail.com>
2017-01-02 14:52:15 -0800

    removed new route and cleaned up debugging code

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ var path = require('path');
 var logger = require('morgan');
 
 var index = require('./routes/index');
-var newRoute = require('./routes/newRoute');
 var app = express();
 
 
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
-app.use('/new', newRoute);
 
 //Get the URL parameter the user has passed in 
 app.get('/:url', function(request, response) {
   var urlParam = request.param('url');
-  console.log("Hello world");
   console.log(urlParam);
 })
 
