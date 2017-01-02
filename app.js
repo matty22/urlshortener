5b4563268e85c1983d8b9986cb1f6321f9d6b4b7
Matt <matt.leonard.22@gmail.com>
2017-01-02 14:32:07 -0800

    removing unneeded dependencies

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-
 var app = express();
 
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 
 //Get the URL parameter the user has passed in 
-app.get('/new/:url', function(request, response) {
+app.get('/:url', function(request, response) {
   var urlParam = request.param('url');
   console.log(urlParam);
 })
