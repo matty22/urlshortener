9478940f4829019f5043d459b2aecb117dffcfba
Matt <matt.leonard.22@gmail.com>
2017-01-04 14:47:53 -0800

    put Mongo stuff inside conditional that checks for valid url param

--- app.js
+++ app.js
@@ -20,34 +20,31 @@ app.use('/', index);
 //Because the user is passing a URL, we must encode it
 app.get('/:location(*)', function(request, response) {
   var urlParam = request.params.location;
-  // if (validUrl.is_http_uri(urlParam) || validUrl.is_https_uri(urlParam)) {
-  //   response.send(urlParam);
-  // } else {
-  //   response.send("Not a valid URL");
-  // }
+  if (validUrl.is_http_uri(urlParam) || validUrl.is_https_uri(urlParam)) {
+    MongoClient.connect(dbUrl, function(err, db) {
+       if (err) {
+          throw err;
+        }
 
-  MongoClient.connect(dbUrl, function(err, db) {
-    if (err) {
-      throw err;
-    }
+        dbOps.insertDocument(db, { original_url: urlParam }, "urlColl", function(results) {
+          response.send(result.ops);
 
-    dbOps.insertDocument(db, { name: "Hello World" }, "urlColl", function(results) {
-      //console.log(result.ops);
-      response.send(result.ops);
+          dbOps.findDocument(db, "urlColl", function(docs) {
+            response.send(docs);
 
-      dbOps.findDocument(db, "urlColl", function(docs) {
-        //console.log(docs);
-        response.send(docs);
-
-        // db.dropCollection("urlColl", function(result) {
-        //   //console.log(result);
-        //   response.send(result);
-        //   db.close();
-        // });
-      });
+            // db.dropCollection("urlColl", function(result) {
+            //   //console.log(result);
+            //   response.send(result);
+            //   db.close();
+            // });
+          });
+        });
     });
+  } else {
+    response.send("Not a valid URL");
+  }
 
-  });
+
 
 });
 
