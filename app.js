9f8121997138adee33f86b00e2bf0db85fda0d15
Matt <matt.leonard.22@gmail.com>
2017-01-05 08:51:34 -0800

    added redirect code

--- app.js
+++ app.js
@@ -28,27 +28,26 @@ app.get('/:location(*)', function(request, response) {
         }
 
         if (!isNaN(Number(urlParam))) {
-          // Search dB for any record that matches the id param
-          // If it exists, response.send that object
-          // Once that works, work on the redirect to the original url
+          // Search for document by ID.
           var urlParamNumber = Number(urlParam);
           dbOps.findOneById(db, urlParamNumber, "urlColl", function(docs) {
-            console.log(docs);
+            //If it exists, redirect to the original_url
             if (docs.length > 0) {
-              response.send(docs[0]);
+              response.writeHead(301, { Location: docs[0].original_url });
             } else {
+              // Else inform user that their shortened url does not exist
               response.send("That shortened url does not exist");
             }
           });
 
         } else if (validUrl.is_http_uri(urlParam) || validUrl.is_https_uri(urlParam)) {
-          // Seach dB for any record that matches the url param
-          // If it exists, reponse.send "This shortened url exists" and return the object
-          // If it does not exist, insert document
+          // Search for document by original_url
           dbOps.findOneByUrl(db, urlParam, "urlColl", function(docs) {
+            // If it already exists, inform user so, and provide the id of that shortened url.
             if (docs.length > 0) {
               response.send("This shortened url exists. Use the id of " + docs[0]._id + " as your url parameter to redirect to that location.");
             } else {
+              // Else add that url to the database as a new record
               dbOps.insertDocument(db, { "original_url": urlParam, "shortened_url": "http://matty22urlshortener.herokuapp.com/" + dbId, "_id": dbId }, "urlColl", function(results) {
                  dbId = dbId + 1;
                  response.send(results.ops);
@@ -56,32 +55,9 @@ app.get('/:location(*)', function(request, response) {
             }
           });
         } else {
+          // If the passed in parameter is not a valid URL, inform users as such.
           response.send("The url parameter is invalid");
         }
-
-
-        // This returns the first match in the dB for the urlParam query
-        // dbOps.findOneByUrl(db, urlParam, "urlColl", function(docs) {
-            
-        //     // If the search finds a match, we don't want to add it again
-        //     // START HERE - Figure out a way to check if the passes param is a number
-        //     // that matches a document ID in the dB
-        //     if (docs.length > 0) {
-        //       response.send("This shortened url exists in the database with id " + docs[0]._id);
-        //     } else if (docs.length == 0 && validUrl.is_http_uri(urlParam) || validUrl.is_https_uri(urlParam)) {
-        //       // If the search does not find a match and the parameter is a valid URL, we want to add it to the database
-        //       dbOps.insertDocument(db, { "original_url": urlParam, "shortened_url": "http://matty22urlshortener.herokuapp.com/" + dbId, "_id": dbId }, "urlColl", function(results) {
-        //         response.send(results.ops);
-        //       });
-              
-        //     } else {
-        //       console.log("THE URL PARAM IS " + urlParam + "and its type is " + typeof urlParam);
-        //       response.send("This is not a valid URL");
-        //     }
-        // });
-        // // This iterator is misplaced again as well. Adding a new document results in duplicate key error
-        // // Must be fixed
-        // dbId = dbId + 1;
     });
 });
 
